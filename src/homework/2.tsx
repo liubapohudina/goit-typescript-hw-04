ae2859ffd20ffa41f4a8df42c7226f3d2314a1ac
Liubov <drogaltseva92@gmail.com>
2024-02-25 17:04:26 +0100

    Update 2.tsx

--- src/homework/2.tsx
+++ src/homework/2.tsx
@@ -6,6 +6,11 @@ type State = {
   isRequestInProgress: boolean;
   requestStep: requestStep;
 }
+/*----------------???ЧИ МОЖНА БУЛО Б ВИКОРИСТАТИ ТАК????-------------------*/
+// type Action = {
+//   type: 'START_REQUEST' | 'PENDING_REQUEST' | 'FINISH_REQUEST' | 'RESET_REQUEST';
+// }
+
 
 type Action = 
   | { type: 'START_REQUEST' }
